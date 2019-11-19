ad3093f40a036861b93c5b4818dc1c573c88acbc
Doug S <DougS@dougs-MacBook-Pro.local>
2019-11-19 08:44:30 +0100

    testing

--- vmafscan/src/index.js
+++ vmafscan/src/index.js
@@ -1,7 +1,7 @@
 //https://www.robinwieruch.de/minimal-node-js-babel-setup
 //great node tutorial
 
-import 'dotenv/config';
+//import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import uuidv4 from 'uuid/v4';
